b2d560d0a35f44ab95c1776f37212e0f1dfeddc2
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-04 13:59:59 -0300

    Criação de nova pasta para Config, e adicionamos uma nova classe QRConfig, e criamos as propriedades dos tipos de QRCode, Size, charsetSource, charsetTarget, Ecc, Color, BgColor, Margin, QZone, Format

---
+++ Entidades/Configurations/QRConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Configurations
+{
+    public class QRConfig
+    {
+        public string Size { get; set; }
+        public string CharsetSource { get; set; }
+        public string  CharsetTarget { get; set; }
+        public char Ecc { get; set; }
+        public string Color { get; set; }
+        public string BgColor { get; set; }
+        public int Margin { get; set; }
+        public int QZone { get; set; }
+        public string Format { get; set; }
+    }
+}
