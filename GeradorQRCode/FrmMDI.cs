8f542ac57eaee3157c723f57a15f00d73729d596
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-04 20:05:33 -0300

    No evento Click, passamos o método de Herança e instanciamos o FrmQRCodeURL para abrir o formulário

--- GeradorQRCode/FrmMDI.cs
+++ GeradorQRCode/FrmMDI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeradorQRCode.Forms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -26,7 +27,7 @@ namespace GeradorQRCode
 
         private void BtnUrlType_Click(object sender, EventArgs e)
         {
-
+            AbrirFormulario(new FrmQRCodeURL());
         }
     }
 }
