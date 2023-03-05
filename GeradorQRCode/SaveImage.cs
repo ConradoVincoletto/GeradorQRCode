ab634e6eef9a14c2a73bda17e11d3f6404de0334
Conrado Vincoletto <conrado.vincoletto@gmail.com>
2023-03-05 20:38:54 -0300

    Criamos uma classe exclusiva para o evento click safe. vai poder salvar a imagem do QRCode, criamos uma condicional para definir as extensões dos formatos das imagens, a classe é estática para não precisar instanciar no evento click, e definimos o título padrão, o diretório, e o nome do arquivo padrão

---
+++ GeradorQRCode/SaveImage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeradorQRCode
+{
+    public static class SaveImage
+    {
+        public static void Save(Image imgQRCode, string format)
+        {
+            SaveFileDialog sfDialog = new SaveFileDialog();
+
+            if (format.Equals("png"))
+            {
+
+            }
+            else if (format.Equals("gif"))
+            {
+
+            }
+            else if (format.Equals("jpeg"))
+            {
+
+            }
+            else if (format.Equals("jpg"))
+            {
+
+            }
+        }
+    }
+}
